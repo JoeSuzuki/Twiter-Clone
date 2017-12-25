d38256f8459d7d210436d2f37c6bf6d7f44ac4c6
JoeSuzuki <joesuzuki516@gmail.com>
2017-12-25 16:59:26 -0500

    base of collectionview

---
+++ Twitter/Twitter/Controllers/HomeController.swift
@@ -0,0 +1,59 @@
+//
+//  HomeController.swift
+//  Twitter
+//
+//  Created by Joe Suzuki on 12/23/17.
+//  Copyright © 2017 Joe Suzuki. All rights reserved.
+//
+
+import UIKit
+
+class WordCell: UICollectionViewCell {
+ 
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupViews()
+    }
+    
+    let wordLabel: UILabel = {
+        let label = UILabel()
+        label.text = "TEST"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    func setupViews() {
+        backgroundColor = .yellow
+        
+        addSubview(wordLabel)
+        wordLabel.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
+        wordLabel.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
+        wordLabel.leftAnchor.constraint(equalTo: self.leftAnchor).isActive = true
+        wordLabel.rightAnchor.constraint(equalTo: self.rightAnchor).isActive = true
+    }
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+class HomeController: UICollectionViewController,UICollectionViewDelegateFlowLayout {
+   
+    let cellId = "cellId"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        collectionView?.backgroundColor = .white
+        collectionView?.register(WordCell.self, forCellWithReuseIdentifier: cellId)
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 4
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath)
+        return cell
+    }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: view.frame.width, height: 50)
+    }
+}
