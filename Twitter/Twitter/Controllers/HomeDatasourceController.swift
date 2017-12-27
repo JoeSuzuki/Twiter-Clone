895cc935b9187790f7079830eaa9228dac0828bb
JoeSuzuki <joesuzuki516@gmail.com>
2017-12-27 01:46:40 -0500

    finalized basic design of Twitter

--- Twitter/Twitter/Controllers/HomeDatasourceController.swift
+++ Twitter/Twitter/Controllers/HomeDatasourceController.swift
@@ -22,7 +22,7 @@ class HomeDatasourceController: DatasourceController {
         return CGSize(width: view.frame.width, height: 50)
     }
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForFooterInSection section: Int) -> CGSize {
-        return CGSize(width: view.frame.width, height: 100)
+        return CGSize(width: view.frame.width, height: 50)
     }
 
 }
