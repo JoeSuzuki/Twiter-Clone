f5dc94b203ad40cb615e6613c82507a4386b6a1f
JoeSuzuki <joesuzuki516@gmail.com>
2017-12-26 22:35:33 -0500

    anchoring the basic view of twitter

--- Twitter/Twitter/Models/HomeDatasource.swift
+++ Twitter/Twitter/Models/HomeDatasource.swift
@@ -9,20 +9,25 @@
 import LBTAComponents
 
 class HomeDatasource: Datasource {
+   
     let words = ["1", "2", "3"]
     
     override func footerClasses() -> [DatasourceCell.Type]? {
         return [UserFooter.self]
     }
+    
     override func headerClasses() -> [DatasourceCell.Type]? {
         return [UserHeader.self]
     }
+    
     override func cellClasses() -> [DatasourceCell.Type] {
         return [UserCell.self]
     }
+    
     override func item(_ indexPath: IndexPath) -> Any? {
         return words[indexPath.item]
     }
+    
     override func numberOfItems(_ section: Int) -> Int {
         return words.count
     }
