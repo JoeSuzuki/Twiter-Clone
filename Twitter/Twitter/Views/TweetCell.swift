10cab0ed7bb1fa17acc6d45897ebff344099a2a9
JoeSuzuki <joesuzuki516@gmail.com>
2017-12-28 16:42:53 -0500

    stackviews for container buttons

--- Twitter/Twitter/Views/TweetCell.swift
+++ Twitter/Twitter/Views/TweetCell.swift
@@ -43,6 +43,18 @@ class TweetCell: DatasourceCell {
         return imageView
     }()
 
+    let replyButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setImage(#imageLiteral(resourceName: "reply"), for: .normal)
+        return button
+    }()
+    
+    let retweetButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setImage(#imageLiteral(resourceName: "retweet"), for: .normal)
+        return button
+    }()
+
     override func setupViews() {
         super.setupViews()
         
@@ -54,8 +66,36 @@ class TweetCell: DatasourceCell {
         addSubview(profileImageView)
         addSubview(messageTextView)
         
+//        addSubview(replyButton)
+//        addSubview(retweetButton)
+        
         profileImageView.anchor(self.topAnchor, left: self.leftAnchor, bottom: nil, right: nil, topConstant: 12, leftConstant: 12, bottomConstant: 0, rightConstant: 0, widthConstant: 50, heightConstant: 50)
+        
         messageTextView.anchor(topAnchor, left: profileImageView.rightAnchor, bottom: bottomAnchor, right: rightAnchor, topConstant: 4, leftConstant: 4, bottomConstant: 0, rightConstant: 0, widthConstant: 0, heightConstant: 0)
+        
+//        replyButton.anchor(nil, left: messageTextView.leftAnchor, bottom: self.bottomAnchor, right: nil, topConstant: 0, leftConstant: 0, bottomConstant: 0, rightConstant: 0, widthConstant: 20, heightConstant: 20)
+        
+        setupBottomButtons()
     }
     
+    fileprivate func setupBottomButtons() {
+        let replyButtonContainerView = UIView()
+        replyButtonContainerView.backgroundColor = .red
+    
+        let retweetButtonContainer = UIView()
+        retweetButtonContainer.backgroundColor = .blue
+        
+        let likeButtonContainerView = UIView()
+        likeButtonContainerView.backgroundColor = .green
+        
+        let directMessageButtonContainer = UIView()
+        directMessageButtonContainer.backgroundColor = .purple
+
+        let buttonStackView = UIStackView(arrangedSubviews: [replyButtonContainerView, retweetButtonContainer, likeButtonContainerView, directMessageButtonContainer])
+        buttonStackView.axis = .horizontal
+        buttonStackView.distribution = .fillEqually
+        
+        addSubview(buttonStackView)
+        buttonStackView.anchor(nil, left: messageTextView.leftAnchor, bottom: self.bottomAnchor, right: self.rightAnchor, topConstant: 0, leftConstant: 0, bottomConstant: 0, rightConstant: 0, widthConstant: 0, heightConstant: 20)
+    }
 }
