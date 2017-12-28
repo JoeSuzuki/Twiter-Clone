fbcc8118ebe61a3fa7a6769eeee52e2ba2525c60
JoeSuzuki <joesuzuki516@gmail.com>
2017-12-27 23:35:03 -0500

    refactored navigationbar code

---
+++ Twitter/Twitter/Controllers/HomeDatasourceController+NavigationBar.swift
@@ -0,0 +1,49 @@
+//
+//  HomeDatasourceController+NavigationBar.swift
+//  Twitter
+//
+//  Created by Joe Suzuki on 12/27/17.
+//  Copyright © 2017 Joe Suzuki. All rights reserved.
+//
+
+import UIKit
+
+extension HomeDatasourceController {
+    
+    func setupNavigationBarItems() {
+        setupRightNavItems()
+        setupLeftNavItems()
+        setupRemainNavItems()
+    }
+    
+    private func setupRemainNavItems() {
+        let tittleImageView = UIImageView(image: #imageLiteral(resourceName: "title_icon"))
+        tittleImageView.frame = CGRect(x: 0, y: 0, width: 34, height: 34)
+        tittleImageView.contentMode = .scaleAspectFit
+        
+        navigationItem.titleView = tittleImageView
+        
+        navigationController?.navigationBar.backgroundColor = .white
+        navigationController?.navigationBar.isTranslucent = false
+    }
+    
+    private func setupLeftNavItems() {
+        let followButton = UIButton(type: .system)
+        followButton.setImage(#imageLiteral(resourceName: "follow").withRenderingMode(.alwaysOriginal), for: .normal)
+        followButton.frame = CGRect(x: 0, y: 0, width: 34, height: 34)
+        
+        navigationItem.leftBarButtonItem = UIBarButtonItem(customView: followButton)
+    }
+    
+    private func setupRightNavItems() {
+        let searchButton = UIButton(type: .system)
+        searchButton.setImage(#imageLiteral(resourceName: "search").withRenderingMode(.alwaysOriginal), for: .normal)
+        searchButton.frame = CGRect(x: 0, y: 0, width: 34, height: 34)
+        
+        let composeButton = UIButton(type: .system)
+        composeButton.setImage(#imageLiteral(resourceName: "compose").withRenderingMode(.alwaysOriginal), for: .normal)
+        composeButton.frame = CGRect(x: 0, y: 0, width: 34, height: 34)
+        
+        navigationItem.rightBarButtonItems = [UIBarButtonItem(customView: composeButton), UIBarButtonItem(customView: searchButton)]
+    }
+}
